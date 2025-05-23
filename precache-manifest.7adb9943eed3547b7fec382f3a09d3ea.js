d6cddf9cb893bd524f82e4ab982144869f41de67
Jay Gala <jay.gala1@somaiya.edu>
2025-05-23 15:30:17 -0400

    Updates

---
+++ precache-manifest.7adb9943eed3547b7fec382f3a09d3ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "69b9044eb431fe82b7cd",
+    "url": "/geofence-editor/static/css/main.4a0fe134.chunk.css"
+  },
+  {
+    "revision": "69b9044eb431fe82b7cd",
+    "url": "/geofence-editor/static/js/main.9d67a15f.chunk.js"
+  },
+  {
+    "revision": "f521cfd0893e242772cc",
+    "url": "/geofence-editor/static/js/runtime~main.e8c2b399.js"
+  },
+  {
+    "revision": "31d3a5a6aa218c17990b",
+    "url": "/geofence-editor/static/js/2.e7d918c8.chunk.js"
+  },
+  {
+    "revision": "6b9b31964bd56d3f04c8bd71ea7501ca",
+    "url": "/geofence-editor/index.html"
+  }
+];
